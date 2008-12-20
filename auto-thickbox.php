fab2b948ed0504708affca952d59bd13a54bcd38
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-20 17:37:55 +0000

    bumping versions except for version checker and sem-pro
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/auto-thickbox/trunk@247 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- auto-thickbox.php
+++ auto-thickbox.php
@@ -4,7 +4,7 @@ Plugin Name: Auto Thickbox
 Plugin URI: http://www.semiologic.com/software/publishing/auto-thickbox/
 Description: Automatically enables thickbox on thumbnail images (i.e. opens them in a fancy pop-up). You can disable the latter feature for individual images by adding the 'nothickbox' class to its anchor tag. To bind images together in a gallery, add an identical rel attribute. The latter is done automatically in each post, on images anchors that have no rel attribute already. Lastly, it lets you open any link in a thickbox by adding the thickbox class to its anchor tag.
 Author: Denis de Bernardy
-Version: 0.1 alpha
+Version: 1.0 beta
 Author URI: http://www.getsemiologic.com
 Update Service: http://version.semiologic.com/plugins
 Update Tag: auto_thickbox
