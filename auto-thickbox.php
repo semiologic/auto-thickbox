2c7a9b2924547e6d6ff3ac5a2881854978c0c038
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-12-16 23:03:29 +0000

    bump
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/auto-thickbox/trunk@3751 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- auto-thickbox.php
+++ auto-thickbox.php
@@ -4,7 +4,7 @@ Plugin Name: Auto Thickbox
 Plugin URI: http://www.semiologic.com/software/auto-thickbox/
 Description: Automatically enables thickbox on thumbnail images (i.e. opens the images in a fancy pop-up).
 Author: Denis de Bernardy
-Version: 2.0.1 beta
+Version: 2.0.1 RC
 Author URI: http://www.getsemiologic.com
 Text Domain: auto-thickbox
 Domain Path: /lang
